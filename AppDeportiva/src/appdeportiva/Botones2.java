101cab254003d1cdc547772738274461a1f46aa6
Eder-Javier <Ederjavier10@gmail.com>
2015-06-01 10:00:36 -0500

    Lógica clase Botones2

--- AppDeportiva/src/appdeportiva/Botones2.java
+++ AppDeportiva/src/appdeportiva/Botones2.java
@@ -5,10 +5,33 @@
  */
 package appdeportiva;
 
+import javax.swing.JButton;
+
 /**
  *
  * @author Eder Javier
  */
+public class Botones2 {
+    
+    private static final String OK = "OK";
+    private static final String CANCELAR = "CANCELAR";
+    private DialogoResultado dialogo;
+    private JButton botonOk;
+    private JButton botonCancelar;
+    
+    public Botones2(DialogoResultado dr){
+        
+        dialogo = dr;
+        botonOk = new JButton("OK");
+        botonOk.setActionCommand(OK);
+        botonOk.addActionListener(this);
+        botonCancelar = new JButton("CANCELAR");
+        botonCancelar.setActionCommand(CANCELAR);
+        botonCancelar.addActionListener(this);
+        add(botonOk);
+        add(botonCancelar);
+    }
+    
+    public void
     
 }
-
