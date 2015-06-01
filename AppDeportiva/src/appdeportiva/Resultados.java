4c941551bc93843b23a02d2a8e2e1e9de4fa76af
Eder-Javier <Ederjavier10@gmail.com>
2015-06-01 12:30:17 -0500

    Corrección de clase Resultados

--- AppDeportiva/src/appdeportiva/Resultados.java
+++ AppDeportiva/src/appdeportiva/Resultados.java
@@ -5,13 +5,45 @@
  */
 package appdeportiva;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
 /**
  *
  * @author Eder Javier
  */
 public class Resultados extends JPanel{
+    
+    private JLabel etiquetaGuion;
+    private JTextField txtGoles1;
+    private JTextField txtGoles2;
 
     public Resultados() {
+        
+        etiquetaGuion = new JLabel("-");
+        txtGoles1 = new JTextField(2);
+        txtGoles2 = new JTextField(2);
+        add(txtGoles1);
+        add(etiquetaGuion);
+        add(txtGoles2);
+        setBorder(new TitledBorder("RESULTADOS"));
+        
+    }
+    
+    public String darGolesEquipo1(){
+        return txtGoles1.getText();
+    }
+    
+    public String darGolesEquipo2(){
+        return txtGoles2.getText();
+    }
+    
+    public void limpiar(){
+        
+        txtGoles1.setText("");
+        txtGoles2.setText("");
     }
     
 }
